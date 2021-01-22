ed4381f4497532533896c842c434b82f01f2ff72
Mohammed Hirzalla <moe@Mohammeds-MacBook-Pro.local>
2021-01-22 12:28:12 -0500

    other pages added need to fix bugs

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import HeroSection from './components/HeroSection';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
+    <>
     <Router>
       <Navbar/>
       <Switch>
         <Route path='/' exact component= {Home}/>
+        <Route path='/services' component={Services}/>
+        <Route path='/products' component={Products}/>
+        <Route path='/signup'  component={SignUp}/>
       </Switch>
     </Router>
+    </>
   );
 }
 
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,18 +8,33 @@ function Cards() {
             <h1>Check out these destinations </h1>   
             <div className='cards_container'>
                 <div className='cards_wrapper'>
+                <ul className="cards__items">
+                        <CardItem
+                        src = "images/image2.png" 
+                        text ="Cardigan Frame 1"
+                        label ="Something dope"
+                        />
+                        <CardItem
+                        src = "images/image3.png" 
+                        text ="Wow so cool"
+                        label ="SUPERRR"
+                        />
+                    </ul>
                     <ul className="cards__items">
                         <CardItem
                         src = "images/image2.png" 
                         text ="Cardigan Frame 1"
+                        label ="Something dope"
                         />
                         <CardItem
-                        src = "images/image2.png" 
-                        text ="Cardigan Frame 1"
+                        src = "images/image3.png" 
+                        text ="Wow so cool"
+                        label ="SUPERRR"
                         />
                         <CardItem
-                        src = "images/image2.png" 
-                        text ="Cardigan Frame 1"
+                        src = "images/image4.png" 
+                        text ="I am A React King"
+                        label="wonderful"
                         />
                     </ul>
                 </div>    
